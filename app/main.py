61bcba2f438bce095f5f590843ce37f1aa90002d
Duayt <poom_tanawat@hotmail.com>
2020-10-06 02:32:46 +0700

    test extract vowel api

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-import kampuan
+import kampuan as kp
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,7 +10,12 @@ async def root():
     return {"message": "Hello World!!!!!!"}
 
 
-@app.get("/greet/{name}")
+@app.get("/user/greet/{name}")
 def kampuan_test(name: str = "Anonymous"):
     return {"input": name,
-            "message": kampuan.test(name=name)}
+            "message": kp.test(name=name)}
+
+
+@app.get("/vowel/{phrase}")
+def extract_vowel(phrase: str = 'สวัสดีครับ'):
+    return kp.extract_vowel(phrase)
--- src/kampuan/__init__.py
+++ src/kampuan/__init__.py
@@ -1,6 +1,8 @@
-from typing import List, Union
+from typing import List, Union, Dict
 import pythainlp.tokenize as tk
-from pythainlp.tokenize import syllable_tokenize 
+from pythainlp.tokenize import syllable_tokenize
+from kampuan.lang_tools import extract_vowel_form
+
 
 def test(name: str = "World!"):
     return f'Hello {name}'
@@ -11,10 +13,19 @@ def tokenize(phrase: str = "สวัสดี") -> List[str]:
     return syllable_tokenize(text=phrase)
 
 
-def puan_kam(phrase: Union[List[str],str]=['กิน','ข้าว'])->List[str]:
+def puan_kam(phrase: Union[List[str], str] = ['กิน', 'ข้าว']) -> List[str]:
 
-    if isinstance(phrase,str):
-        phrase=tokenize(phrase=phrase)
-    
+    if isinstance(phrase, str):
+        phrase = tokenize(phrase=phrase)
 
     return phrase
+
+
+def extract_vowel(phrases: str) -> Dict:
+    if isinstance(phrases, str):
+        phrases = tokenize(phrase=phrases)
+    results = {}
+    for word in phrases:
+        results[word] = extract_vowel_form(word).vowel_form
+
+    return results
