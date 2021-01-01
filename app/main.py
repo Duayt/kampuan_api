bae0eb79c92f865194b4c639b26d78356c4b5566
Tanawat Chiewhawan <poom_tanawat@hotmail.com>
2021-01-02 00:19:12 +0700

    bot event

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ from .const import ALL_CONST
 
 # variables
 ENV = 'puan'
+
 CHANNEL_SECRET = str(os.getenv('CHANNEL_SECRET'))
 CHANNEL_ACCESS_TOKEN = str(os.getenv('CHANNEL_ACCESS_TOKEN'))
 GOOGLE_APPLICATION_CREDENTIALS = str(
