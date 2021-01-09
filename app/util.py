6a13b41c8c7c07bff94e1767957042dd64b3781f
Tanawat Chiewhawan <poom_tanawat@hotmail.com>
2021-01-10 01:49:13 +0700

    polish

--- app/util.py
+++ app/util.py
@@ -9,15 +9,15 @@ class SourceInfo:
 
     @classmethod
     def new(cls, env):
-        return cls('new', False, env)
+        return cls('new', True, env)
 
     @classmethod
     def rejoin(cls, env):
-        return cls('rejoin', False, env)
+        return cls('rejoin', True, env)
 
     @classmethod
     def old(cls, env):
-        return cls('old_room', False, env)
+        return cls('old_room', True, env)
 
     def to_dict(self):
         return asdict(self)
